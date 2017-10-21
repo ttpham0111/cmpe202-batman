94d4d71a96fd1442c84a3ef265e60df517eb5f9c
amanvojha <amanvojha@icloud.com>
2017-10-20 20:05:11 -0700

    Added remaining files for Desert Map

---
+++ Learning_Code/Assests/Maps and Objects/Desert_Theme/Final_Desert_Map/cactus_1.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Cactus" tilewidth="108" tileheight="111" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="108" height="111" source="../../../../../../GitHub/cmpe202-batman/Learning_Code/Assests/Maps and Objects/Desert_Theme/png/Objects/Cactus (1).png"/>
+ </tile>
+</tileset>
---
+++ Learning_Code/Assests/Maps and Objects/Desert_Theme/Final_Desert_Map/untitled6.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="6" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../../../../../GitHub/cmpe202-batman/Learning_Code/Assests/Maps and Objects/Desert_Theme/png/Tile/6.png"/>
+ </tile>
+</tileset>
