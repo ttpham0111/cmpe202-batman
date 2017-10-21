83b660f52ef1c934f05065cedbca3f4b66e4e0cb
amanvojha <amanvojha@icloud.com>
2017-10-21 01:44:27 -0700

    Created Room Theme Map

---
+++ Learning_Code/Assests/Maps and Objects/Room_Theme/Image.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Room" tilewidth="64" tileheight="64" tilecount="70" columns="10">
+ <image source="Room.jpg" width="640" height="480"/>
+</tileset>
