0926a288a0cf012affab6a0f5388da369ae6eb43
amanvojha <amanvojha@icloud.com>
2017-10-27 02:26:06 -0700

    Created Room with smaller pixels

--- Learning_Code/Assests/Maps and Objects/Room_Theme/Image.tsx
+++ Learning_Code/Assests/Maps and Objects/Room_Theme/Image.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Room" tilewidth="64" tileheight="64" tilecount="70" columns="10">
+<tileset name="Room" tilewidth="32" tileheight="32" tilecount="300" columns="20">
  <image source="Room.jpg" width="640" height="480"/>
 </tileset>
