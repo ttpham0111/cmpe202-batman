38b3e7e5a0f2a6acc1a573cd5f6ec29972b198c7
Ankit Bharadiya <asbharadiya@gmail.com>
2017-10-30 20:17:44 -0700

    character added, character animation on movement, closes #55

--- Learning_Code/movement_test/index.js
+++ Learning_Code/movement_test/index.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload () {
 
-    game.load.image('player', 'player.gif');
+    game.load.spritesheet('player', 'player1.png', 64, 64);
     
 }
 
@@ -19,21 +19,19 @@ function create () {
 
     game.stage.backgroundColor = "#eee";
 
-    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    player = game.add.sprite(32, 32, 'player', 27);
 
     game.physics.arcade.enable(player);
 
-    player.width = 50;
-    player.height = 50;
-
-    player.anchor.setTo(0.5,0.5);
+    player.anchor.setTo(0.5);
     
     isMoving = false;
     direction = directions[0];
 
-    player.animations.add('player_walk');
-
-    //cursors = game.input.keyboard.createCursorKeys();
+    player.animations.add('up', [1, 2, 3, 4, 5, 6, 7, 8], 10, true);
+    player.animations.add('left', [10, 11, 12, 13, 14, 15, 16, 17], 10, true);
+    player.animations.add('down', [19, 20, 21, 22, 23, 24, 25, 26], 10, true);
+    player.animations.add('right', [28, 29, 30, 31, 32, 33, 34, 35], 10, true);
 
     game.input.keyboard.onUpCallback = function(e){
         if(e.keyCode === Phaser.Keyboard.LEFT){
@@ -50,12 +48,16 @@ function moveCharacter(steps){
         var new_pos;
         if(direction === 'left'){
             new_pos = { x: player.position.x-(32*steps), y: player.position.y };
+            player.frame = 9;
         } else if(direction === 'right'){
             new_pos = { x: player.position.x+(32*steps), y: player.position.y };
+            player.frame = 27;
         } else if(direction === 'up'){
             new_pos = { x: player.position.x, y: player.position.y-(32*steps) };
+            player.frame = 0;
         } else {
             new_pos = { x: player.position.x, y: player.position.y+(32*steps) };
+            player.frame = 18;
         }
         isMoving = true;
         tween = game.add.tween(player).to(new_pos, 300, Phaser.Easing.Linear.None, true);
@@ -70,9 +72,25 @@ function moveCharacterComplete(){
 
 function update () {
     if (isMoving) {
-        // play sprite at 12 frames per second
-        player.animations.play('player_walk', 12, true);
+        if(direction === 'left'){
+            player.animations.play('left');
+        } else if(direction === 'right'){
+            player.animations.play('right');
+        } else if(direction === 'up'){
+            player.animations.play('up');
+        } else {
+            player.animations.play('down');
+        }
     } else {
+        if(direction === 'left'){
+            player.frame = 9;
+        } else if(direction === 'right'){
+            player.frame = 27;
+        } else if(direction === 'up'){
+            player.frame = 0;
+        } else {
+            player.frame = 18;
+        }
         player.animations.stop();
     }
     if (game.input.activePointer.isDown)
