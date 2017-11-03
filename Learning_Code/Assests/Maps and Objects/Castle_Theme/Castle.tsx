07845c08a51b527bf3f9a04e2f4665a3501c0dc2
amanvojha <amanvojha@icloud.com>
2017-11-03 16:54:23 -0700

    Added Castle Map

---
+++ Learning_Code/Assests/Maps and Objects/Castle_Theme/Castle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Castle" tilewidth="16" tileheight="16" tilecount="132" columns="11">
+ <image source="Castle.png" width="176" height="192"/>
+</tileset>
