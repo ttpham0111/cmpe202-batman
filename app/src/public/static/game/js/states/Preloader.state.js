73b7cd8df861917bd33a22480c8fdf315a107dce
Jessie Qiu <qqqbbbjjj@gmail.com>
2017-11-10 00:03:04 +0000

    update the preloader

--- app/src/public/static/game/js/states/Preloader.state.js
+++ app/src/public/static/game/js/states/Preloader.state.js
@@ -3,6 +3,8 @@ Game.Preloader = function() {};
 Game.Preloader.prototype = {
   preload: function() {
     game.load.spritesheet('hero', 'public/static/game/assets/hero.png', 64, 64);
+    game.load.image("ivy", "public/static/game/assets/enemies/ivy.png");
+    game.load.image("riddler", "public/static/game/assets/enemies/riddler.png");
   },
 
   create: function() {
