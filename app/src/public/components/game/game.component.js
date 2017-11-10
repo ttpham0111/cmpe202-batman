8e8f25492d832d50baa432b41d6eaf0520ca983f
Tuan Pham <tpham@ancestry.com>
2017-11-10 15:52:44 -0800

    Use closures instead of assigning self

--- app/src/public/components/game/game.component.js
+++ app/src/public/components/game/game.component.js
@@ -14,19 +14,18 @@ Vue.component('todo-game-screen', {
   },
 
   mounted: function() {
-    const self = this;
     const gameEl = this.$refs.game;
-    game = new Phaser.Game(Game.WIDTH, Game.HEIGHT, Phaser.AUTO, gameEl);
+    const game = new Phaser.Game(Game.WIDTH, Game.HEIGHT, Phaser.AUTO, gameEl);
 
     game.state.add(Game.states.BOOT, new Game.Boot());
     game.state.add(Game.states.PRELOADER, new Game.Preloader());
     game.state.add(Game.states.LEVEL_1, new Game.Level1(this.editor));
     game.state.start(Game.states.BOOT);
 
-    game.state.onStateChange.add(function(stateKey) {
+    game.state.onStateChange.add((stateKey) => {
       const state = game.state.states[stateKey];
-      if (state.showTutorial) self.$emit('show-tutorial');
-      self.$emit('change', state);
+      if (state.showTutorial) this.$emit('show-tutorial');
+      this.$emit('change', state);
     });
   }
 });
