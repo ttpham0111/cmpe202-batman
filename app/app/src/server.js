30c9e3f3f86424ae018dc7e71959932734ef9282
Tuan Pham <tpham@ancestry.com>
2017-11-18 15:33:10 -0800

    Move app directory out one level

--- app/app/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const config = require('./config');
-
-
-app.listen(config.port, config.host, null, function () {
-  console.log('Listening on port ' + config.port);
-});
